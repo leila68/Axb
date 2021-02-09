235dfe6af73da08359b887da52a8fb0f740f56ea
DESKTOP-DARSAH7\Leila <leila.cheshmi@gmail.com>
2021-02-09 15:13:10 +0330

    updated TriangularSolve.cpp

--- TriangularSolve.cpp
+++ TriangularSolve.cpp
@@ -35,3 +35,23 @@ Matrix* TriangularSolve::solve(Matrix *L, Matrix *d)
 
     return y;
 }
+Matrix* TriangularSolve::solve(CSR *L, Matrix *d)
+{
+    Matrix *y = new Matrix(rowNo, 1, "y");
+    double s = 0;
+
+
+    y->setArray(0, 0, d->getArray(0, 0)/ L-> val[0])  ;
+    for (int i=1; i<rowNo; i++)
+    {
+        for (int j=L->ptr[i]; j<L->ptr[i+1]-1; j++ )
+        {
+            s = L->val[j]* y->getArray(L->idx[j],0) + s;
+
+        }
+        y->setArray(i, 0, (d->getArray(i, 0) - s));
+        y->setArray(i, 0, y->getArray(i, 0) / L->val[L->ptr[i]+i]);//is diagonal
+        s = 0;
+    }
+    return  y;
+}

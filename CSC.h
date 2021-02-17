6ea40bfd2428fb4aec77dfe5e51de8f65f37176a
DESKTOP-DARSAH7\Leila <leila.cheshmi@gmail.com>
2021-02-17 16:03:01 +0330

    updated CSC.h

--- CSC.h
+++ CSC.h
@@ -4,10 +4,28 @@
 
 #ifndef AXB_CSC_H
 #define AXB_CSC_H
+#include "Matrix.h"
+
+class CSC
+{
+  int row = 0;
+  int col = 0;
+  int nonzero = 0;
+  int *ptr;
+  int *idx;
+  double *val;
+  CSC(int r, int c, int nnz);
+
+    CSC(Matrix *m);
 
 
-class CSC {
 
+public:
+    CSC(int r, int c, int nnz, int *p, int *idx, double *val);
+
+    void Triplet();
+
+    Matrix* turnToRegular();
 };
 
 

0b819cd89986c53526123f828e20ff5798173bbe
DESKTOP-DARSAH7\Leila <leila.cheshmi@gmail.com>
2021-03-05 15:21:56 +0330

    updated diagonal.h

--- diagonal.h
+++ diagonal.h
@@ -4,11 +4,28 @@
 
 #ifndef AXB_DIAGONAL_H
 #define AXB_DIAGONAL_H
+#include "Matrix.h"
+
+    class diagonal
+    {
+        int dNum= 0 ;
+        int row=0;
+        int col=0;
+        int *offset;
 
 
-class diagonal {
+    public:
 
-};
+        Matrix* turnTo(Matrix *d);
+        Matrix* multiply(Matrix *d);
+
+        diagonal(int r, int c, int d);
+    };
 
 
 #endif //AXB_DIAGONAL_H
+
+
+
+
+
